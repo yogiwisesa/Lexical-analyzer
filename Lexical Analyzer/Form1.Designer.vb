dec4192cc5283973b677b801c2a0453671e4cf96
yogiwisesa <yogiwisesa@gmail.com>
2016-10-20 14:46:28 +0700

    Main Engine
    this is the main engine of my lexical analyzer, this analyzer will
    generate the token from the user's input, ex: if, iff, xor, and as a
    operators and p,q,r,s as a operands.

---
+++ Lexical Analyzer/Form1.Designer.vb
@@ -0,0 +1,73 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnGenerate = New System.Windows.Forms.Button()
+        Me.lblToken = New System.Windows.Forms.Label()
+        Me.txtInput = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'btnGenerate
+        '
+        Me.btnGenerate.Location = New System.Drawing.Point(132, 226)
+        Me.btnGenerate.Name = "btnGenerate"
+        Me.btnGenerate.Size = New System.Drawing.Size(140, 23)
+        Me.btnGenerate.TabIndex = 0
+        Me.btnGenerate.Text = "Generate Token"
+        Me.btnGenerate.UseVisualStyleBackColor = True
+        '
+        'lblToken
+        '
+        Me.lblToken.AutoSize = True
+        Me.lblToken.Location = New System.Drawing.Point(13, 91)
+        Me.lblToken.Name = "lblToken"
+        Me.lblToken.Size = New System.Drawing.Size(10, 13)
+        Me.lblToken.TabIndex = 1
+        Me.lblToken.Text = "-"
+        '
+        'txtInput
+        '
+        Me.txtInput.Location = New System.Drawing.Point(16, 50)
+        Me.txtInput.Name = "txtInput"
+        Me.txtInput.Size = New System.Drawing.Size(256, 20)
+        Me.txtInput.TabIndex = 2
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 261)
+        Me.Controls.Add(Me.txtInput)
+        Me.Controls.Add(Me.lblToken)
+        Me.Controls.Add(Me.btnGenerate)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnGenerate As Button
+    Friend WithEvents lblToken As Label
+    Friend WithEvents txtInput As TextBox
+End Class
---
+++ Lexical Analyzer/Form1.vb
@@ -0,0 +1,194 @@
+﻿Public Class Form1
+    Private Sub btnGenerate_Click(sender As Object, e As EventArgs) Handles btnGenerate.Click
+        Dim sText, token As String ' sText stands for stringText - Deklarasi Variabel
+        Dim i, length As Integer ' Deklarasi Variabel
+        sText = txtInput.Text + " " 'Memeasukan isi TextBoxInput ke variabel string (sText) & fungsi ditambah spasi sebagai penanda agar token keluar karena setiap token dikeluarkan bila sText.chars(i) = " " 
+        sText = sText.ToLower ' << set ke lower agar tidak case sensitive
+        length = sText.Length
+        i = 0
+
+Space:
+        On Error Resume Next
+        If i < length Then
+            Select Case sText.Chars(i)
+                Case "p", "q", "r", "s" ' PQRS
+                    i = i + 1
+                    Select Case sText.Chars(i)
+                        Case " "
+                            i = i + 1
+                            token = token + " 1" ' Token dikeluarkan saat sText.chars(i) = " " agar mengetahui apakan suatu sintaks tidak ada lanjutannya, misal ifg agar menghasilkan error bukan 6error
+                            GoTo Space
+                        Case Else
+                            token = token + "error"
+                    End Select
+                Case "n" ' NOT
+                    i = i + 1
+                    Select Case sText.Chars(i)
+                        Case "o"
+                            i = i + 1
+                            Select Case sText.Chars(i)
+                                Case "t"
+                                    i = i + 1
+                                    Select Case sText.Chars(i)
+                                        Case " "
+                                            token = token + " 2"
+                                            i = i + 1
+                                            GoTo Space
+                                        Case "("
+                                            token = token + " 2 9"
+                                            i = i + 1
+                                            GoTo Space
+                                        Case Else
+                                            token = token + "error"
+                                    End Select
+                                Case Else
+                                    token = token + "error"
+                            End Select
+                        Case Else
+                            token = token + "error"
+                    End Select
+                Case "a" ' AND
+                    i = i + 1
+                    Select Case sText.Chars(i)
+                        Case "n"
+                            i = i + 1
+                            Select Case sText.Chars(i)
+                                Case "d"
+                                    i = i + 1
+                                    Select Case sText.Chars(i)
+                                        Case " "
+                                            token = token + " 3"
+                                            i = i + 1
+                                            GoTo Space
+                                        Case Else
+                                            token = token + "error"
+                                    End Select
+                                Case Else
+                                    token = token + "error"
+                            End Select
+                        Case Else
+                            token = token + "error"
+                    End Select
+                Case "o" ' OR
+                    i = i + 1
+                    Select Case sText.Chars(i)
+                        Case "r"
+                            i = i + 1
+                            Select Case sText.Chars(i)
+                                Case " "
+                                    i = i + 1
+                                    token = token + " 4"
+                                    GoTo Space
+                                Case Else
+                                    token = token + "error"
+                            End Select
+                        Case Else
+                            token = token + "error"
+                    End Select
+                Case "x" ' XOR
+                    i = i + 1
+                    Select Case sText.Chars(i)
+                        Case "o"
+                            i = i + 1
+                            Select Case sText.Chars(i)
+                                Case "r"
+                                    i = i + 1
+                                    Select Case sText.Chars(i)
+                                        Case " "
+                                            i = i + 1
+                                            token = token + " 5"
+                                            GoTo Space
+                                        Case Else
+                                            token = token + "error"
+                                    End Select
+                                Case Else
+                                    token = token + "error"
+                            End Select
+                        Case Else
+                            token = token + "error"
+                    End Select
+                Case "i" ' IF
+                    i = i + 1
+                    Select Case sText.Chars(i)
+                        Case "f"
+                            i = i + 1
+                            Select Case sText.Chars(i)
+                                Case " "
+                                    i = i + 1
+                                    token = token + " 6"
+                                    GoTo Space
+                                Case "f"
+                                    i = i + 1
+                                    Select Case sText.Chars(i)
+                                        Case " "
+                                            i = i + 1
+                                            token = token + " 8"
+                                            GoTo Space
+                                        Case Else
+                                            token = token + "error"
+                                    End Select
+                                Case Else
+                                    token = token + "error"
+                            End Select
+                        Case Else
+                            token = token + "error"
+                    End Select
+                Case "t" ' THEN
+                    i = i + 1
+                    Select Case sText.Chars(i)
+                        Case "h"
+                            i = i + 1
+                            Select Case sText.Chars(i)
+                                Case "e"
+                                    i = i + 1
+                                    Select Case sText.Chars(i)
+                                        Case "n"
+                                            i = i + 1
+                                            token = token + " 7"
+                                            Select Case sText.Chars(i)
+                                                Case " "
+                                                    i = i + 1
+                                                    GoTo Space
+                                                Case Else
+                                                    token = token + "error"
+                                            End Select
+                                        Case Else
+                                            token = token + "error"
+                                    End Select
+                                Case Else
+                                    token = token + "error"
+                            End Select
+                        Case Else
+                            token = token + "error"
+                    End Select
+                Case "(" ' GROUPING
+                    i = i + 1
+                    Select Case sText.Chars(i)
+                        Case " "
+                            token = token + " 9"
+                            i = i + 1
+                            GoTo Space
+                        Case Else
+                            token = token + "error"
+                    End Select
+
+                Case ")" ' GROUPING JAGA- JAGA KALAU ADA GROUPING SETELAH GROUPING
+                    i = i + 1
+                    Select Case sText.Chars(i)
+                        Case " "
+                            token = token + " 10"
+                            i = i + 1
+                            GoTo Space
+                        Case Else
+                            token = token + "error"
+                    End Select
+                Case " "
+                    token = token
+                Case Else
+                    token = token + "error"
+            End Select
+        End If
+
+        lblToken.Text = token 'Set LabelToken menjadi token
+    End Sub
+End Class
